19b2da4cb88d249ce0066891983126fbb0166188
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2020-11-16 18:00:29 +0100

    Data réteg kész

---
+++ ISGUOH_FELEVES/Data/PlayersDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+
+namespace Data
+{
+    public class PlayersDbContext : DbContext
+    {
+        public DbSet<Player> Jatekosok { get; set; }
+        public DbSet<Team> Csapatok { get; set; }
+        public DbSet<League> Liga { get; set; }
+        public PlayersDbContext(DbContextOptions<PlayersDbContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.
+                    UseLazyLoadingProxies().
+                    UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\Player.mdf;integrated security=True;MultipleActiveResultSets=True");
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            modelbuilder.Entity<Player>(entity =>
+            {
+                entity
+                .HasOne(Player => Player.Csapat)
+                .WithMany(Team => Team.Jatekosok)
+                .HasForeignKey(Player => Player.TeamID);
+            });
+            modelbuilder.Entity<Team>(entity =>
+            {
+                entity
+                .HasOne(Team => Team.League)
+                .WithMany(League => League.Teams)
+                .HasForeignKey(Team => Team.LeagueID);
+            });
+        }
+
+    }
+}
