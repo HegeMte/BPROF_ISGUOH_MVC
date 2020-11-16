8aa086d8409de74ac1906305099e6cd513bbed81
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2020-11-16 19:36:27 +0100

    TeamLogic kesz

---
+++ ISGUOH_FELEVES/Logic/TeamLogic.cs
@@ -0,0 +1,51 @@
+﻿using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    class TeamLogic
+    {
+        IRepository<Player> playerrepo;
+        IRepository<Team> teamrepo;
+        IRepository<League> leaguerepo;
+
+        public TeamLogic(IRepository<Player> playerrepo, IRepository<Team> teamrepo, IRepository<League> leaguerepo)
+        {
+            this.playerrepo = playerrepo;
+            this.leaguerepo = leaguerepo;
+            this.teamrepo = teamrepo;
+        }
+
+
+        public void AddTeam(Team team)
+        {
+            this.teamrepo.Add(team);
+
+        }
+
+        public void DeleteTeam(Team team)
+        {
+            this.teamrepo.Delete(team);
+
+        }
+
+        public IQueryable<Team> GetAllTeam()
+        {
+            return teamrepo.Read();
+        }
+
+        public Team GetTeam(string TeamID)
+        {
+            return teamrepo.Read(TeamID);
+        }
+
+        public void UpdateTeam(string oldteam,Team newteam)
+        {
+            teamrepo.Update(oldteam, newteam);
+        }
+    }
+}
