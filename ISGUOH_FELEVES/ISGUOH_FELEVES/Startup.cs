4d026fa15985102e5fb88427bd30458c5dbc4aa0
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2020-11-16 19:47:26 +0100

    Transient hozzaadva

--- ISGUOH_FELEVES/ISGUOH_FELEVES/Startup.cs
+++ ISGUOH_FELEVES/ISGUOH_FELEVES/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Logic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,10 @@ namespace ISGUOH_FELEVES
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<PlayerLogic, PlayerLogic>();
+            services.AddTransient<TeamLogic, TeamLogic>();
+            services.AddTransient<LeagueLogic, LeagueLogic>();
+
             services.AddMvc(opt => opt.EnableEndpointRouting = false);
 
         }
