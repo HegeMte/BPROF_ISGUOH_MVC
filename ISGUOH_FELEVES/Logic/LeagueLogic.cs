128f5c075115e864f6354db70718661e3ba85bc5
Hegedűs Máté <hegedus.mate@stud.uni-obuda.hu>
2020-11-16 19:43:32 +0100

    LeagueLogic kesz

---
+++ ISGUOH_FELEVES/Logic/LeagueLogic.cs
@@ -0,0 +1,52 @@
+﻿using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    class LeagueLogic
+    {
+        IRepository<Player> playerrepo;
+        IRepository<Team> teamrepo;
+        IRepository<League> leaguerepo;
+
+        public LeagueLogic(IRepository<Player> playerrepo, IRepository<Team> teamrepo, IRepository<League> leaguerepo)
+        {
+            this.teamrepo = teamrepo;
+            this.playerrepo = playerrepo;
+            this.leaguerepo = leaguerepo;
+        }
+
+        public void AddLeague(League league)
+        {
+            this.leaguerepo.Add(league);
+        }
+
+        public void DeleteLeague(string LeagueID)
+        {
+            this.leaguerepo.Delete(LeagueID);
+        }
+
+
+        public IQueryable<League> GetAllLeague()
+        {
+            return leaguerepo.Read();
+        }
+
+        public League GetLeague (string LeagueID)
+        {
+            return leaguerepo.Read(LeagueID);
+        }
+
+        public void UpdateLeague(string oldLeague, League newLeague)
+        {
+
+            leaguerepo.Update(oldLeague, newLeague);
+        }
+
+
+    }
+}
